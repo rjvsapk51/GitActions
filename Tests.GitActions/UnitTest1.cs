3287efae6a688227e9d36e04fde41496b283f74b
Rajiv Sapkota <rajiv.sapkota@dogmagroup.co.uk>
2022-08-12 08:26:01 +0545

    Initial commit

---
+++ Tests.GitActions/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tests.GitActions
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
